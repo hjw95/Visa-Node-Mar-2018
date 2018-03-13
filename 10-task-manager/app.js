c5b46408034c68a08caa9f30c61530fd7fd7d3b6
Magesh <tkmagesh77@gmail.com>
2018-03-13 15:18:57 +0800

    task manager implementation in express

--- 10-task-manager/app.js
+++ 10-task-manager/app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var taskRoutes = require('./routes/taskRoutes');
+var taskApiRoutes = require('./routes/taskApiRoutes');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/tasks', taskRoutes);
+app.use('/tasks/api', taskApiRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ 10-task-manager/routes/taskApiRoutes.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+
+var taskList = [
+	{
+		id : 1,
+		name : 'Learn JavaScript',
+		desc : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem necessitatibus illo fugit blanditiis vitae, voluptate possimus aut placeat doloremque quam quia quibusdam ipsa harum error facere. Dolore itaque praesentium eius.',
+		isCompleted : false
+	},
+	{
+		id : 2,
+		name : 'Explore Singapore',
+		desc : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem necessitatibus illo fugit blanditiis vitae, voluptate possimus aut placeat doloremque quam quia quibusdam ipsa harum error facere. Dolore itaque praesentium eius.',
+		isCompleted : false
+	}
+];
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	var viewModel = { tasks : taskList };
+  	res.json(viewModel);
+});
+/*
+router.get('/new', function(req, res, next){
+	res.render('tasks/new');
+});
+
+router.post('/new', function(req, res, next){
+	var newId = taskList.reduce(function(prevResult, task){
+		return task.id > prevResult ? task.id : prevResult
+	}, 0) + 1;
+	var newTask = {
+		id : newId,
+		name: req.body.txtTaskName,
+		desc : req.body.txtTaskDesc,
+		isCompleted : false
+	};
+	taskList.push(newTask);
+	res.redirect('/tasks');
+});
+
+router.get('/details/:id', function(req, res, next){
+	var taskId = parseInt(req.params.id);
+	var result = taskList.find(function(task){
+		return task.id === taskId;
+	});
+	if (result){
+		var viewData = { task : result };
+		res.render('tasks/details', viewData);
+	} else {
+		res.redirect('/tasks');	
+	}
+})
+*/
+module.exports = router;
